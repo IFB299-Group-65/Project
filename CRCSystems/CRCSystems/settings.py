6cae51eb2db84377bba54e41f2b85d225b71c253
kikimutiara <31962693+kikimutiara@users.noreply.github.com>
2018-09-20 10:33:54 +1000

    Add files via upload

--- CRCSystems/CRCSystems/settings.py
+++ CRCSystems/CRCSystems/settings.py
@@ -28,6 +28,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'crc.ifb299@gmail.com'
+EMAIL_HOST_PASSWORD = 'ifb299-65'
 
 # Application definition
 
