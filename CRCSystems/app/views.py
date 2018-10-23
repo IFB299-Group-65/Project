5ced95fcb9a838ae549e23d1a401da17f8b86288
kikimutiara <31962693+kikimutiara@users.noreply.github.com>
2018-10-23 15:42:14 +1000

    Update views.py

--- CRCSystems/app/views.py
+++ CRCSystems/app/views.py
@@ -8,52 +8,92 @@ from django.template import RequestContext
 from datetime import datetime
 from django.core.mail import send_mail
 from django.conf import settings
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
+from django.http import HttpResponseRedirect
+
+from app.models import *
 
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/index.html',
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        }
+        'app/home.html',
     )
 
-def contact(request):
-    """Renders the contact page."""
+def price(request):
+    """Renders the price page."""
+    assert isinstance(request, HttpRequest)
+    f = ''
+    if(request.GET.get('Submit')):
+        f = Car.objects.filter(car_makename=request.GET.get('car_make'))
+    return render(
+        request,
+        'app/price.html',
+        {'filter' : f},
+    )
+
+def payment(request):
+    """Renders the payment page."""
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/contact.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
+        'app/payment.html',
     )
 
-def about(request):
-    """Renders the about page."""
+def booking(request):
+    """Renders the booking page."""
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
+        'app/booking.html',
     )
 
-def email(request):
-    """Sends email automatically"""
-    subject = 'Lost Property at Car Rental Company'
-    message = 'Hi, \n\nWe found your belongings from the last car that you rent from CRC, Please call us to schedule your pick up time. Thank You.\n\nRegards, \n\nCRC Manager'
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['kiki.mutiara49@yahoo.com',]
+def terms(request):
+    """Renders the terms and condition page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/terms-and-conditions.html',
+    )
 
-    send_mail( subject, message, email_from, recipient_list )
+def tracking(request):
+    """Renders the tracking page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/tracking.html',
+    )
 
-    return HttpResponse('<h1>Email sent!</h1>')
+def report(request):
+    """Render Car report page"""
+    assert isinstance(request, HttpRequest)
+    o = ''
+    if(request.GET.get('mybtn')):
+        if(request.GET.get('method') == 'Picked Up'):
+            o = Orders.objects.filter(order_pickupdate__month=int(request.GET.get('month')))
+        
+        #o = Order.objects.filter(method=request.GET.get('method'), location=request.GET.get('location'), pub_date__month=int(request.GET.get('month')))
+        o = str(len(o))
+#        o = Order.objects.all()
+    return render(
+        request, 
+        'app/report.html',
+        {'o': o},
+    )
+
+def login(request):
+    """Render Car report page"""
+    assert isinstance(request, HttpRequest)
+    o = 0
+    if(request.GET.get('Submit')):
+        o = AuthUser.objects.filter(username=request.GET.get('username'), password=make_password(request.GET.get('password'), '1'))
+        o = (len(o) == 1)
+        
+        return HttpResponseRedirect('report.html')
+    return render(
+        request, 
+        'app/login.html',
+    )
+
