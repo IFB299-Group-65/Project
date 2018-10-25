d9fe50e6990b804bb4419eb72dad5b8516f2d6b6
Michaela-Andrae <amichaelaandrae@hotmail.com>
2018-10-25 10:08:14 +1000

    Updating pushers to CRCchat
    Updating pushers to CRCchat

---
+++ CRC (chat)/CRCchat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CRCchatConfig(AppConfig):
+    name = 'CRCchat'
---
+++ CRC (chat)/app/views.py
@@ -0,0 +1,2 @@
+#render library for returning views to the browser
+from django.shortcuts import render
